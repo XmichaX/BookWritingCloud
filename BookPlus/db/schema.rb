d22bffff4ca6c3eb4f2a6a9c4a25c8076e5a6ff7
Michael F <free-humbus@gmx.de>
2014-04-29 20:50:38 +0200

    Bootstrap added to the project

--- BookPlus/db/schema.rb
+++ BookPlus/db/schema.rb
@@ -22,19 +22,19 @@ ActiveRecord::Schema.define(:version => 20140426131124) do
   end
 
   create_table "users", :force => true do |t|
-    t.string "email", :default => "", :null => false
-    t.string "encrypted_password", :default => "", :null => false
-    t.string "reset_password_token"
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", :default => 0, :null => false
+    t.integer  "sign_in_count",          :default => 0,  :null => false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.string "full_name"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.string   "full_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
