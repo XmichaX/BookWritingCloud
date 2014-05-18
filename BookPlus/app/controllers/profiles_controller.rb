bde5d7b284af4116baf6432113ce561363dd4561
Beate Jost <Beate.Jost@iais.fraunhofer.de>
2014-05-18 21:42:35 +0200

    repair profile stuff (profile is not a model or controller, just a name for user)

--- BookPlus/app/controllers/profiles_controller.rb
+++
@@ -1,9 +0,0 @@
-class ProfilesController < ApplicationController
-  before_filter :authenticate_user!, except: [:index]
-
-  def show
-    @user_profile = current_user
-    @user_profile_books = current_user.books
-  end
-
-end
--- BookPlus/db/schema.rb
+++ BookPlus/db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140504105612) do
+ActiveRecord::Schema.define(:version => 20140513094905) do
 
   create_table "books", :force => true do |t|
-    t.string   "title"
+    t.string "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
@@ -24,20 +24,33 @@ ActiveRecord::Schema.define(:version => 20140504105612) do
     t.integer "user_id"
   end
 
+  create_table "chunks", :force => true do |t|
+    t.string "title"
+    t.string "section"
+    t.text "content"
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "chunks", ["book_id"], :name => "index_chunks_on_book_id"
+  add_index "chunks", ["user_id"], :name => "index_chunks_on_user_id"
+
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
+    t.string "email", :default => "", :null => false
+    t.string "encrypted_password", :default => "", :null => false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0,  :null => false
+    t.integer "sign_in_count", :default => 0, :null => false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "full_name"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.string "full_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
